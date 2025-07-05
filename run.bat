e3c9bf5c46b797377d0add287b15efa9d5da9914
Stellar_Celestial <mengseanggamming@gmail.com>
2025-07-05 21:58:50 +0700

    structure

---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+python ./src/app.py
---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, render_template
+from scrapers.worldometers.scraper import scrape_country_population  # make sure folder name matches
+
+app = Flask(__name__, template_folder='./templates')
+
+@app.route("/api/<source>/<category>/<target>", methods=["GET"])
+def get_population(source, category, target):
+    country = target.strip().lower()  # use the URL path param directly
+    if not country:
+        return jsonify({
+            "error": "Missing 'target' URL parameter",
+            "source": source,
+            "category": category,
+            "target": target
+        }), 400
+    try:
+        if source.lower() != "worldometers" or category.lower() != "population":
+            return jsonify({"code": 400,
+                            "error": "Unsupported source or category",
+                            "source": source,
+                            "category": category,
+                            "target": target
+                            }), 400
+
+        data = scrape_country_population(country)
+        return jsonify({"data": data})
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 404
+    except Exception as e:
+        print(f"[ERROR] {e}")  # Log server-side error
+        return jsonify({"error": "Internal server error"}), 500
+
+@app.route("/")
+def home():
+    return render_template('index.html')  # loads src/templates/index.html
+
+@app.route("/api")
+def api():
+    return render_template('api/index.html')  # loads src/templates/api/index.html
+
+if __name__ == "__main__":
+    app.run(debug=True)
